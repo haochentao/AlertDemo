b4deb343b2f4bc2bfbab7541b63bb83207beaaaf
Your Name <you@example.com>
2013-09-12 10:17:48 +0800

    Initial Commit

---
+++ AlertDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AlertDemo
+//
+//  Created by 郝辰涛 on 13-9-12.
+//  Copyright (c) 2013年 郝辰涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AlertDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AlertDemo
+//
+//  Created by 郝辰涛 on 13-9-12.
+//  Copyright (c) 2013年 郝辰涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
